d609deb96c825c2318107f4c78f57db694b37d59
Art Boer <art.boer@phpro.be>
2021-03-22 17:04:56 +0100

    Single act and multiple assert

--- tests/Integration/TrainBuilderTest.php
+++ tests/Integration/TrainBuilderTest.php
@@ -46,6 +46,7 @@ final class TrainBuilderTest extends TestCase
 
         foreach ($train as $wagon) {
             $carnivore = $wagon->hasCarnivore();
+            $this->assertGreaterThanOrEqual(0, $wagon->getSpaceLeft());
 
             if (!$carnivore) {
                 continue;
@@ -59,45 +60,7 @@ final class TrainBuilderTest extends TestCase
                 $this->assertGreaterThan($carnivore->getSpace(), $animal->getSpace());
             }
         }
-    }
 
-    public function testNoWagonIsOverfilled(): void
-    {
-        $train = $this->trainBuilder->getTrain();
-
-        foreach ($train as $wagon) {
-            $this->assertGreaterThanOrEqual(0, $wagon->getSpaceLeft());
-        }
-    }
-
-    public function testCanFillTwelveWagons(): void
-    {
-        $this->assertCount(12, $this->trainBuilder->getTrain(), 'Train does not contain 12 wagons!');
-    }
-
-    public function testFirstWagonContainsCarnivore(): void
-    {
-        $firstWagon = $this->trainBuilder->getTrain()[0];
-        $firstAnimal = $firstWagon->getAnimals()[0];
-        $this->assertEquals('Carnivore', $firstAnimal->getDiet());
-    }
-
-    public function testLastWagonContainsThreeAnimals(): void
-    {
-        $train = $this->trainBuilder->getTrain();
-        $lastWagon = end($train);
-
-        $this->assertCount(3, $lastWagon->getAnimals(), 'Last wagon does not contain 3 animals!');
-    }
-
-    public function testLastWagonContainsThreeSmallHerbivores(): void
-    {
-        $train = $this->trainBuilder->getTrain();
-        $lastWagon = end($train);
-
-        foreach ($lastWagon->getAnimals() as $animal) {
-            $this->assertEquals('Small', $animal->getSize());
-            $this->assertEquals('Herbivore', $animal->getDiet());
-        }
+        $this->assertCount(12, $train, 'Train does not contain 12 wagons!');
     }
 }
